47d5f6f88101e4466932163c060133c838f9dc40
qstyle <ordin404@gmail.com>
2020-05-19 15:57:21 +0300

    feat(form) add js

---
+++ source/js/menu.js
@@ -0,0 +1,19 @@
+const DISPLAY_NONE = "display-none";
+
+const menu = document.querySelector(".page-nav__wrapper");
+const closeButton = document.querySelector(".page-nav__button--on");
+const openButton = document.querySelector(".page-nav__button--off");
+
+openButton.addEventListener('click', function(event) {
+    event.preventDefault();
+    menu.classList.remove(DISPLAY_NONE);
+    closeButton.classList.remove(DISPLAY_NONE);
+    openButton.classList.add(DISPLAY_NONE);
+});
+
+closeButton.addEventListener('click', function(event) {
+    event.preventDefault();
+    menu.classList.add(DISPLAY_NONE);
+    closeButton.classList.add(DISPLAY_NONE);
+    openButton.classList.remove(DISPLAY_NONE);
+});
